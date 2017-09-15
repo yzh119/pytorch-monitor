a4e2587578b3a3bb924709d119b4abddebcab509
Zihao Ye <expye@outlook.com>
2017-09-15 20:01:14 +0800

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='training_monitor',
+    version='0.1',
+    packages=['training_monitor'],
+    url='https://github.com/yzh119/training_monitor',
+    license='',
+    author='expye',
+    author_email='expye@outlook.com',
+    description='Visualize training process'
+)
